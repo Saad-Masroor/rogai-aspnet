ab2b4192dcb566a5d907310eb5d8fa4fab9085f1
Saad-Masroor <realsaadyt@gmail.com>
2026-02-21 15:48:52 +0500

    feat(project-setup): scaffold ROGAI project with EF Core, Controllers, and wwwroot

---
+++ src/ROAGI.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ROAGI.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
