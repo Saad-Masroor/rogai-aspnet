e6cb608c3e9feac31cf170b8d537bb9e655617e8
Saad-Masroor <realsaadyt@gmail.com>
2026-02-27 11:59:52 +0500

    feat(database): configure EF Core and add User entity
    - Added DefaultConnection in appsettings.json
    - Created User model
    - Implemented ApplicationDbContext
    - Registered DbContext in Program.cs

---
+++ src/ROAGI.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ROAGI.Web.Models;
+
+namespace ROAGI.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ src/ROAGI.Web/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace ROAGI.Web.Models
+{
+    public class User
+    {
+        public int user_id { get; set; }
+
+        public required string username { get; set; }
+
+        public required string password_hash { get; set; }
+
+        public DateTime created_at { get; set; }
+
+        public DateTime? updated_at { get; set; }
+
+        public bool is_active { get; set; }
+    }
+}
